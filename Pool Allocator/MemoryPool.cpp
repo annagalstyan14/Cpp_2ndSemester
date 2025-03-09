513c833dd0c79216b3c35ed55ac98846a81652c3
Anna Galstyan <43260520+annagalstyan14@users.noreply.github.com>
2025-03-09 15:55:59 +0400

    Update MemoryPool.cpp
    Added Thread Safety, deleting individual objects, proper memory allignment and functions to monitor memory pools usage

--- Pool Allocator/MemoryPool.cpp
+++ Pool Allocator/MemoryPool.cpp
@@ -1,31 +1,64 @@
 #include "MemoryPool.h"
 #include <new>
+#include <stdexcept>
 
-MemoryPool::MemoryPool(std::size_t objSize) : currentIndex(0), objectSize(objSize) {
-	allocateNewBlock();
+MemoryPool::MemoryPool(std::size_t objSize, std::size_t blockSize) 
+    : BlockSize(blockSize), currentIndex(0), objectSize(objSize) {
+    allocateNewBlock();
 }
 
 MemoryPool::~MemoryPool() {
-	for (void* block : blocks) {
-		::operator delete(block);
-	}
+    try {
+        for (void* block : blocks) {
+            ::operator delete(block);
+        }
+    } catch (...) {
+    }
 }
 
 void* MemoryPool::allocate() {
-	if (currentIndex >= BlockSize / objectSize) {
-		allocateNewBlock();
-	}
-	void* ptr = static_cast<char*>(blocks.back()) + (currentIndex * objectSize);
-	currentIndex++;
-	return ptr;
+    std::lock_guard<std::mutex> lock(mtx);
+
+    if (!freeList.empty()) {
+        void* ptr = freeList.back();
+        freeList.pop_back();
+        return ptr;
+    }
+
+    if (currentIndex >= BlockSize / objectSize) {
+        allocateNewBlock();
+    }
+
+    void* ptr = static_cast<char*>(blocks.back()) + (currentIndex * objectSize);
+    currentIndex++;
+    return ptr;
+}
+
+void MemoryPool::deallocate(void* ptr) {
+    std::lock_guard<std::mutex> lock(mtx);
+    freeList.push_back(ptr);
 }
 
 void MemoryPool::deallocateAll() {
-	currentIndex = 0;
+    std::lock_guard<std::mutex> lock(mtx); 
+    currentIndex = 0;
+    freeList.clear();
 }
 
 void MemoryPool::allocateNewBlock() {
-	void* newBlock = ::operator new(BlockSize);
-	blocks.push_back(newBlock);
-	currentIndex = 0;
+    void* newBlock = ::operator new(BlockSize, std::align_val_t{alignof(std::max_align_t)});
+    if (!newBlock) {
+        throw std::bad_alloc(); 
+    }
+
+    blocks.push_back(newBlock);
+    currentIndex = 0;
+}
+
+std::size_t MemoryPool::getAllocatedBlocks() const {
+    return blocks.size();
+}
+
+std::size_t MemoryPool::getFreeBlocks() const {
+    return freeList.size();
 }
