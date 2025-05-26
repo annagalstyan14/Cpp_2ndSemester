ecc8e883acfa6f5f361f9890117690990ff1e0c3
Anna Galstyan <43260520+annagalstyan14@users.noreply.github.com>
2025-05-26 17:45:46 +0400

    Update main.cpp

--- Grep/main.cpp
+++ Grep/main.cpp
@@ -3,7 +3,12 @@
 
 int main(int argc, char* argv[]) {
     GrepOptions options;
-    options.parse(argc, argv);
+    options.parseArgs(argc, argv);
+
+    if (options.patterns.empty() && !options.files.empty()) {
+        options.patterns.push_back(options.files.front());
+        options.files.erase(options.files.begin());
+    }
 
     GrepEngine engine(options);
     engine.searchFiles(options.files);
