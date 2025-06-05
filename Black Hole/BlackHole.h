a325f53f3d60f33410ab9e69cba90be873918bd0
Anna Galstyan <anulgal04@gmail.com>
2025-06-05 11:23:55 +0400

    declaration

--- Black Hole/BlackHole.h
+++ Black Hole/BlackHole.h
@@ -7,4 +7,17 @@
 const double G = 6.67430e-11;
 const double c = 2.99792458e8;
 
+struct Particle{
+    double x, y, z;
+    double vx, vy, vz;
+    double mass;
+};
+
+class BlackHole{
+
+};
+
+void updateParticle();
+void saveTrajectory();
+
 #endif
