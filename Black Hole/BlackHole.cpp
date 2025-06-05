773b2f61189a4c20ab4218ff7a6e3bcf842c83de
Anna Galstyan <anulgal04@gmail.com>
2025-06-05 11:30:15 +0400

    source file

---
+++ Black Hole/BlackHole.cpp
@@ -0,0 +1,16 @@
+#include "BlackHole.h"
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+BlackHole::BlackHole() : {}
+
+bool BlackHole::isInsideEventHorizon() const {}
+
+void BlackHole::getAcceleration() const {}
+
+double BlackHole::getDeflectionAngle() const {}
+
+void updateParticle() {}
+
+void saveTrajectory() {}
