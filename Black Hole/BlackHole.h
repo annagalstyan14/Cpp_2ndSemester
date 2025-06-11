05075f289148121262f7ea07087292ab4f740d6d
Anna Galstyan <anulgal04@gmail.com>
2025-06-11 16:09:43 +0400

    .

--- Black Hole/BlackHole.h
+++ Black Hole/BlackHole.h
@@ -3,7 +3,6 @@
 
 #include <vector>
 #include <string>
-#include <iostream>
 
 const double G = 6.67430e-11;
 const double c = 2.99792458e8;
@@ -21,7 +20,7 @@ class BlackHole{
     double rs;
     
     public:
-    BlackHole(double x0 = 0, double y0 = 0, double z0 = 0, double m = 1.989 * pow(10,30));
+    BlackHole(double x0, double y0, double z0, double m);
     bool BlackHole::isInsideEventHorizon(double px, double py, double pz) const;
     void BlackHole::getAcceleration(double px, double py, double pz, double& ax, double& ay, double& az) const;
     double BlackHole::getDeflectionAngle(double b) const;
